728a1c23c7940a961b5830b5ab724e4502e8bafe
sharknell <103021663+sharknell@users.noreply.github.com>
2023-09-04 10:50:36 +0900

    Add files via upload

---
+++ WebSite/src/test/java/com/web/website/WebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
