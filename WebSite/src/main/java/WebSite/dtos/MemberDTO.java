a64d646f198853a785508776b410ac17aff5f7e1
kimm9487 <137483871+kimm9487@users.noreply.github.com>
2023-09-05 11:09:01 +0900

    Add files via upload

---
+++ WebSite/src/main/java/WebSite/dtos/MemberDTO.java
@@ -0,0 +1,25 @@
+package WebSite.dtos;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MemberDTO {
+	private String id;
+	private String pw;
+	private String phoneNum;
+	private LocalDateTime regDate;
+	private LocalDateTime modDate;
+	private String name;
+	private String email;
+}
