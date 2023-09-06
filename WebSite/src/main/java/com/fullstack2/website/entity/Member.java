5163f5842a0ec756db1b6eda7c5e02b6b1b7dbec
kimm9487 <137483871+kimm9487@users.noreply.github.com>
2023-09-06 12:43:28 +0900

    카테고리별 entity 빌더

--- WebSite/src/main/java/com/fullstack2/website/entity/Member.java
+++ WebSite/src/main/java/com/fullstack2/website/entity/Member.java
@@ -3,6 +3,8 @@ package com.fullstack2.website.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,19 +19,19 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class Member extends BaseEntity {
-    @Id
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(length = 45, nullable = false)
-    private String id; // 주요 식별자로 Long 타입 사용
-
+    private Long id;//회원 번호, 이걸로 로그인하는게 아니라 자동 생성됨
+    
     @Column(length = 45, nullable = false)
     private String pw;
     
     @Column(length = 45, nullable = false)
     private String name;
     
-    @Id
     @Column(length = 45, nullable = false, unique = true)
-    private String email;
+    private String email;//로그인은 이걸로
 
     @Column(length = 45, nullable = false)
     private String phoneNum;
@@ -37,7 +39,7 @@ public class Member extends BaseEntity {
     @Column(length = 45, nullable = false)
     private String addr;//집주소
     
-    @Column(length = 45, nullable = false)
+    @Column(length = 45)
     private String landline;//일반전화
     
     @Column(length = 45, nullable = false)
