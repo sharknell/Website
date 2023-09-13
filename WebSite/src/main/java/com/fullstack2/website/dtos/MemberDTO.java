87290c603056c8e9e3d4666ce73d7505ab8f5e0a
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-09-13 14:44:50 +0900

    회원관리 dto 들 만지지말기 만질 시 말하고 만지기

--- WebSite/src/main/java/com/fullstack2/website/dtos/MemberDTO.java
+++ WebSite/src/main/java/com/fullstack2/website/dtos/MemberDTO.java
@@ -1,7 +1,8 @@
-package com.fullstack2.website.dtos;
+package com.fullstack2.webSite.dtos;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,18 +14,24 @@ import lombok.ToString;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class MemberDTO {
-	private Long id;//회원 번호, 이걸로 로그인하는게 아니라 자동 생성됨
-	private String pw;
-	private String phoneNum;//핸드폰
-	private LocalDateTime regDate;
-	private LocalDateTime modDate;
-	private String userName;
-	private String email;//로그인은 이걸로
-	private String addrBasic;//집주소
-	private String addrCode;//집주소
-	private String addrRest;//집주소
-	private String landline;//일반전화
-	private String birth;//생년월일
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String postalCode;
+    private String addressBasic;
+    private String addressRest;
+    private String type;
+    private String phoneArea;
+    private String phoneNumber;
+    private String phoneExt;
+    private String mobileArea;
+    private String mobileNumber;
+    private String mobileExt;
+    private String birthYear; // 수정: 생년
+    private String birthMonth; // 수정: 생월
+    private String birthDay; // 수정: 생일
+    
+    private String role;
 }
---
+++ WebSite/src/main/java/com/fullstack2/website/dtos/MemberJoinDto.java
@@ -0,0 +1,33 @@
+package com.fullstack2.webSite.dtos;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberJoinDto {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String postalCode;
+    private String addressBasic;
+    private String addressRest;
+    
+    
+    private String mobile;
+    
+    private String birth; // 수정: 생년
+   
+    
+    private String role;
+}
