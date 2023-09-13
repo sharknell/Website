a78121650907cf7b1da0ca8a51bf8062c8f4d43e
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-09-13 14:42:44 +0900

    @requestMappinrg("/my") 로 설정 변경 시 나한테 말하고 바꾸기 시큐리티 설정 걸림

--- WebSite/src/main/java/com/fullstack2/website/controller/MyController.java
+++ WebSite/src/main/java/com/fullstack2/website/controller/MyController.java
@@ -1,9 +1,11 @@
-package com.fullstack2.website.controller;
+package com.fullstack2.webSite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/my")
 public class MyController {
 
     @GetMapping("/")
