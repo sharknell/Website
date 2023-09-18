ae0af0bc2b976f492a497e5d2284043f0f5d5d28
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-09-18 12:27:58 +0900

    QnA게시판 DTO
    QnA게시판 DTO 클래스

---
+++ WebSite/src/main/java/com/fullstack2/website/dtos/QnAPageRequestDTO.java
@@ -0,0 +1,34 @@
+package com.fullstack2.webSite.dtos;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+/*
+ * 화면에서 사용자가 요청하는 QnA 페이지 번호를 분석해서
+ * 그 번호에 해당하는 글 목록 갯수 정보를 담는 DTO
+ */
+@Builder
+@AllArgsConstructor
+@Data
+public class QnAPageRequestDTO {
+
+	private int page;
+	private int size;
+	
+	private String type;//검색타입
+	private String keyword;//검색 키워드
+	
+	public QnAPageRequestDTO() {
+		this.page = 1;
+		this.size = 10;
+	}
+	
+	public Pageable getPageable(Sort sort) {
+		return PageRequest.of(page - 1, size, sort);
+	}
+}
