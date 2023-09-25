ee211711ce8b9647523db9a01a69c77431e55351
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-09-25 15:13:11 +0900

    컨트롤러
    테스트를 위해 컨트롤러 수정

--- WebSite/src/main/java/com/fullstack2/website/controller/TestController.java
+++ WebSite/src/main/java/com/fullstack2/website/controller/TestController.java
@@ -1,6 +1,10 @@
 package com.fullstack2.webSite.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,39 +12,96 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fullstack2.webSite.dtos.QnADTO;
+import com.fullstack2.webSite.dtos.QnAPageRequestDTO;
+import com.fullstack2.webSite.dtos.QnAPageResultDTO;
+import com.fullstack2.webSite.dtos.ReviewDTO;
 import com.fullstack2.webSite.dtos.ReviewPageRequestDTO;
+import com.fullstack2.webSite.dtos.ReviewPageResultDTO;
+import com.fullstack2.webSite.entity.QnA;
 import com.fullstack2.webSite.entity.Review;
+import com.fullstack2.webSite.repository.QnARepository;
 import com.fullstack2.webSite.repository.ReviewRepository;
+import com.fullstack2.webSite.service.QnAService;
 import com.fullstack2.webSite.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
 public class TestController {
 	
-	@Autowired
 	private final ReviewService reviewService;
-	
-	@Autowired
 	private final ReviewRepository reviewRepository;
+	private final QnAService qnaService;
+	private final QnARepository qnaRepository;
 	
-	@GetMapping("/test")
-	public void test(@ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO, Model model) {
-		//Model에게 review란에 뿌려질 항목 DTO를 전달함.
-		model.addAttribute("result", reviewService.getList(reviewPageRequestDTO));
+	@Autowired // 생성자에 대한 자동 주입 어노테이션
+	public TestController(
+			ReviewService reviewService,
+			ReviewRepository reviewRepository,
+			QnAService qnaService,
+			QnARepository qnaRepository) {
+		this.reviewService = reviewService;
+		this.reviewRepository = reviewRepository;
+		this.qnaService = qnaService;
+		this.qnaRepository = qnaRepository;
 	}
 	
-	@PostMapping("/addReview")
-	public String addReview(@RequestParam String text, @RequestParam String reviewer) {
-		// 엔티티 생성 및 데이터 저장
+	@GetMapping("/test")
+	public void test(@ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO,
+	                 @ModelAttribute("qnaPageRequestDTO") QnAPageRequestDTO qnaPageRequestDTO,
+	                 Model model) {
+	    // Review 정보와 QnA 정보를 각각 사용하여 작업 수행
+	    ReviewPageResultDTO<ReviewDTO, Review> reviewResult = reviewService.getList(reviewPageRequestDTO);
+	    QnAPageResultDTO<QnADTO, QnA> qnaResult = qnaService.getList(qnaPageRequestDTO);
+	    
+	    // 모델에 결과 추가
+	    model.addAttribute("reviewResult", reviewResult);
+	    model.addAttribute("qnaResult", qnaResult);
+	}
+
+    @PostMapping("/addReview")
+    public String addReview(@RequestParam String text, @ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO) {
+        // 현재 로그인한 사용자의 정보를 가져옵니다.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 인증 정보에서 사용자 이름을 가져옵니다.
+        String reviewer = authentication.getName();
+
+        // 엔티티 생성 및 데이터 저장
         Review review = new Review();
         review.setText(text);
         review.setReviewer(reviewer);
+        review.setModDate(LocalDateTime.now());
+        review.setRegDate(LocalDateTime.now());
 
         // 리뷰 저장
         reviewRepository.save(review);
 
+        // 리뷰를 추가한 후 검색 타입을 다시 설정
+        reviewPageRequestDTO.setType("리뷰");
+
         return "redirect:/test"; // 리다이렉트할 페이지 지정
+    }
+
+	@PostMapping("/addQnA")
+	public String addQnA(@RequestParam String text, @ModelAttribute("qnaPageRequestDTO") QnAPageRequestDTO qnaPageRequestDTO) {
+		//현재 로그인 한 사용자의 정보를 가져옴
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		//인증 정보에서 사용자 이름을 가져옴
+		String questioner = authentication.getName();
+		
+		//엔티티 생성 및 데이터 저장
+		QnA qna = new QnA();
+		qna.setText(text);
+		qna.setQuestioner(questioner);
+		qna.setModDate(LocalDateTime.now());
+		qna.setRegDate(LocalDateTime.now());
+		
+		qnaRepository.save(qna);
+		
+		return "redirect:/test";
 	}
 }
--- WebSite/src/main/java/com/fullstack2/website/controller/UserController.java
+++ WebSite/src/main/java/com/fullstack2/website/controller/UserController.java
@@ -1,84 +1,196 @@
 package com.fullstack2.webSite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fullstack2.webSite.dtos.ReviewDTO;
+import com.fullstack2.webSite.dtos.ReviewPageRequestDTO;
+import com.fullstack2.webSite.dtos.ReviewPageResultDTO;
+import com.fullstack2.webSite.entity.Review;
+import com.fullstack2.webSite.repository.ReviewRepository;
+import com.fullstack2.webSite.service.ReviewService;
 
 @Controller
+@RequestMapping("/my")
 public class UserController {
 
+	private final ReviewService reviewService;
 	
-
-	@GetMapping("/cart")
-	public String cartPage() {
-		// 장바구니 페이지 로직 처리
-		return "cart"; // cart.html 페이지로 이동
+	@Autowired // 생성자에 대한 자동 주입 어노테이션
+	public UserController(ReviewService reviewService){
+		this.reviewService = reviewService;
 	}
 
-	@GetMapping("/sns")
-	public String snsPage() {
-		// SNS 페이지 로직 처리
-		return "sns"; // sns.html 페이지로 이동
-	}
+	@GetMapping("/login")
+   public String loginPage() {
+      // 로그인 페이지 로직 처리
+      return "login"; // login.html 페이지로 이동
+   }
 
-	
+   @GetMapping("/join")
+   public String joinPage() {
+      // 회원 가입 페이지 로직 처리
+      return "join"; // join.html 페이지로 이동
+   }
 
-	@GetMapping("/aboutus")
-	public String aboutUs() {
-		// About Us 페이지 로직 처리
-		return "aboutUs"; // AboutUs.html 페이지로 이동
-	}
+   @GetMapping("/mypage")
+   public String mypage() {
+      // 회원 가입 페이지 로직 처리
+      return "mypage"; // mypage.html 페이지로 이동
+   }
 
-	@GetMapping("/onlinestore")
-	public String onlineStore() {
-		// Online Store 페이지 로직 처리
-		return "onlinestore"; // online-store.html 페이지로 이동
-	}
+   @GetMapping("/cart")
+   public String cartPage() {
+      // 장바구니 페이지 로직 처리
+      return "cart"; // cart.html 페이지로 이동
+   }
 
-	@GetMapping("/collection")
-	public String collection() {
-		// Collection 페이지 로직 처리
-		return "collection"; // collection.html 페이지로 이동
-	}
+   @GetMapping("/sns")
+   public String snsPage() {
+      // SNS 페이지 로직 처리
+      return "sns"; // sns.html 페이지로 이동
+   }
 
-	@GetMapping("/community")
-	public String community() {
-		// Community 페이지 로직 처리
-		return "redirect:/board/list";
-	}
+   @GetMapping("/")
+   public String home() {
+      return "home"; // 홈 페이지
+   }
 
-	@GetMapping("/newarrivals")
-	public String newArrivals() {
-		// NEW ARRIVALS 서브메뉴 페이지 로직 처리
-		return "newarrivals"; // newarrivals.html 페이지로 이동
-	}
+   @GetMapping("/main")
+   public String main() {
+      return "main"; // 메인 페이지
+   }
 
+   @GetMapping("/aboutus")
+   public String aboutUs() {
+      // About Us 페이지 로직 처리
+      return "aboutUs"; // AboutUs.html 페이지로 이동
+   }
 
-	@GetMapping("/orderhistory")
-	public String orderhistoryPage() {
-		// SNS 페이지 로직 처리
-		return "orderhistory"; // manageacc.html 페이지로 이동
-	}
+   @GetMapping("/onlinestore")
+   public String onlineStore() {
+      // Online Store 페이지 로직 처리
+      return "onlinestore"; // online-store.html 페이지로 이동
+   }
 
-	
+   @GetMapping("/collection")
+   public String collection() {
+      // Collection 페이지 로직 처리
+      return "collection"; // collection.html 페이지로 이동
+   }
 
-	@GetMapping("/points")
-	public String pointsPage() {
-		// SNS 페이지 로직 처리
-		return "points"; // manageacc.html 페이지로 이동
-	}
+   @GetMapping("/newarrivals")
+   public String newArrivals() {
+      // NEW ARRIVALS 서브메뉴 페이지 로직 처리
+      return "newarrivals"; // newarrivals.html 페이지로 이동
+   }
 
-	@GetMapping("/manageposts")
-	public String managepostscPage() {
-		// SNS 페이지 로직 처리
-		return "manageposts"; // manageacc.html 페이지로 이동
-	}
+   @GetMapping("/orderhistory")
+   public String orderhistoryPage() {
+      // SNS 페이지 로직 처리
+      return "orderhistory"; // manageacc.html 페이지로 이동
+   }
 
+   @GetMapping("/manageacc")
+   public String manageaccPage() {
+      // SNS 페이지 로직 처리
+      return "manageacc"; // manageacc.html 페이지로 이동
+   }
 
-	
-	@GetMapping("/cart2")
-	public String cart2Page() {
-		// SNS 페이지 로직 처리
-		return "cart2"; // manageacc.html 페이지로 이동
-	}
+   @GetMapping("/points")
+   public String pointsPage() {
+      // SNS 페이지 로직 처리
+      return "points"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/manageposts")
+   public String managepostscPage() {
+      // SNS 페이지 로직 처리
+      return "manageposts"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/managename")
+   public String managenamePage() {
+      // SNS 페이지 로직 처리
+      return "managename"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/managepw")
+   public String managepwPage() {
+      // SNS 페이지 로직 처리
+      return "managepw"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/findpassword")
+   public String testpasswordPage() {
+      // SNS 페이지 로직 처리
+      return "findpassword"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/findpassword_drag")
+   public String testpassword_dragPage() {
+      // SNS 페이지 로직 처리
+      return "findpassword_drag"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/findemail")
+   public String findemailPage() {
+      // SNS 페이지 로직 처리
+      return "findemail"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/managemobile")
+   public String managemobilePage() {
+      // SNS 페이지 로직 처리
+      return "managemobile"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/manageaddress")
+   public String manageaddressPage() {
+      // SNS 페이지 로직 처리
+      return "manageaddress"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/withdraw")
+   public String withdrawPage() {
+      // SNS 페이지 로직 처리
+      return "withdraw"; // manageacc.html 페이지로 이동
+   }
+
+   @GetMapping("/cart2")
+   public String cart2Page() {
+      // SNS 페이지 로직 처리
+      return "cart2"; // manageacc.html 페이지로 이동
+   }
+   @GetMapping("/outer")
+   public String outerPage() {
+      // SNS 페이지 로직 처리
+      return "outer"; // manageacc.html 페이지로 이동
+   }
+   
+   @GetMapping("/outer/outer1")
+   public String outer1Page() {
+      // SNS 페이지 로직 처리
+      return "/outer/outer1"; // manageacc.html 페이지로 이동
+   }
+   
+   @GetMapping("/notice")
+   public String notice() {
+	   return "notice";
+   }
+   
+   @GetMapping("/review")
+   public String review(@ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO, Model model) {
+
+	   ReviewPageResultDTO<ReviewDTO, Review> reviewResult = reviewService.getList(reviewPageRequestDTO);
+	   
+	   model.addAttribute("reviewResult", reviewResult);
+	   
+	   return "review";
+   }
 
 }
