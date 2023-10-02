95311bd408bd4cbd1b224ba25f41568525e6bce1
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-10-02 12:54:49 +0900

    Add files via upload

--- WebSite/src/main/java/com/fullstack2/website/config/SpringSecurityConfig.java
+++ WebSite/src/main/java/com/fullstack2/website/config/SpringSecurityConfig.java
@@ -1,17 +1,33 @@
 package com.fullstack2.webSite.config;
 
 import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.fullstack2.webSite.oauth2.OAuth2Service;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.awt.PageAttributes.MediaType;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfig {
+    @Autowired
+    private final OAuth2Service oAuth2UserService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -20,15 +36,20 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable()
+        http.csrf().disable()
                 .authorizeHttpRequests(request -> request
                         .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .requestMatchers("/status", "/images/**", "/view/join", "/auth/join","/my/","/main").permitAll()
-                        .requestMatchers("/static/**").permitAll()
+                        .requestMatchers("/status", "/static/**", "/view/join", "/auth/join","/my/","/my/main").permitAll()
+                        .requestMatchers("/findEmail","/resultFindEmail","/findPw").permitAll()
                         .requestMatchers("/my/**").permitAll()
+                        
+                        
+                        .requestMatchers("/view/admin").hasRole("ADMIN")
+                        
+                       
                         .anyRequest().authenticated()
                 )
-                .formLogin(login -> login
+               .formLogin(login -> login
                         .loginPage("/view/login")
                         .loginProcessingUrl("/login-process")
                         .usernameParameter("email")
@@ -36,11 +57,21 @@ public class SpringSecurityConfig {
                         .defaultSuccessUrl("/view/mainLog", true)
                         .permitAll()
                 )
-                
-                .logout()
-                .logoutUrl("/logout")
-                .invalidateHttpSession(true);
+               .oauth2Login(oauth2Configurer -> oauth2Configurer
+                       .loginPage("/view/login")
+                       
+                       .defaultSuccessUrl("/view/mainLog2", true) // 로그인 성공시 이동할 URL
+                       .userInfoEndpoint()// 사용자가 로그인에 성공하였을 경우,
+                       
+                       .userService(oAuth2UserService)// 해당 서비스 로직을 타도록 설정
+			)
+               .logout()
+	         .logoutUrl("/logout")
+	          .invalidateHttpSession(true);
+     
 
         return http.build();
     }
+   
+    
 }
