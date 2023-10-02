cbba5c474b0b78ce4178c3e83f27332390542a1e
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-10-02 13:11:28 +0900

    Add files via upload

--- WebSite/src/main/java/com/fullstack2/website/controller/ModifyMemberInfoController.java
+++ WebSite/src/main/java/com/fullstack2/website/controller/ModifyMemberInfoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fullstack2.webSite.dtos.MemberJoinDto;
+import com.fullstack2.webSite.oauth2.UserProfile;
 import com.fullstack2.webSite.repository.MemberQuery;
 
 import jakarta.servlet.http.HttpSession;
@@ -18,177 +19,322 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/info")
 public class ModifyMemberInfoController {
-    private MemberQuery query;
-    private PasswordEncoder passwordEncoder;
+	private MemberQuery query;
+	private PasswordEncoder passwordEncoder;
 
-    @Autowired // passwordEncoder 주입
-    public ModifyMemberInfoController(MemberQuery query, PasswordEncoder passwordEncoder) {
-        this.query = query;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @GetMapping("/mypage")
-    public String mypage(Model model, HttpSession session) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-        model.addAttribute("dto", dto);
-        return "mypage";
-    }
-
-    @GetMapping("/manageacc")
-    public String manageacc(Model model, HttpSession session) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-        model.addAttribute("dto", dto);
-        return "manageacc";
-    }
-
-    @GetMapping("/myinfo")
-    public String myinfo(Model model, HttpSession session) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-        model.addAttribute("dto", dto);
-        return "myinfo";
-    }
-
-    @GetMapping("/managepw")
-    public String managepw(Model model, HttpSession session) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-        model.addAttribute("dto", dto);
-        return "managepw";
-    }
-
-    @PostMapping("/managepw")
-    public String changePw(
-            @RequestParam("password") String password,
-            @RequestParam("newPassword") String newPassword,
-            @RequestParam("confirmPassword") String confirmPassword,
-            Model model,
-            HttpSession session
-            , RedirectAttributes redirectAttributes
-            ) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-
-        // 현재 비밀번호와 저장된 비밀번호를 비교하여 일치하는지 확인
-        if (passwordEncoder.matches(password, dto.getPassword())) {
-            // 새 비밀번호와 확인 비밀번호가 일치하는지 확인
-            if (newPassword.equals(confirmPassword)) {
-                // 새 비밀번호를 암호화하여 저장
-                String hashedNewPassword = passwordEncoder.encode(newPassword);
-                query.updatePassword(hashedNewPassword, dto.getPassword());
-                dto.setPassword(hashedNewPassword);
-                model.addAttribute("dto", dto);
-                redirectAttributes.addAttribute("message", "비밀번호변경완료");
-
-                // 비밀번호 변경 성공 시 로그아웃 후 메인 페이지로 리다이렉트
-                return "redirect:/view/login";
-            }
-        }
-        
-        // 비밀번호 변경 실패 시 변경 페이지로 다시 이동
-        model.addAttribute("dto", dto);
-        return "redirect:/info/managepw";
-    }
-
-    @GetMapping("/managemobile")
-    public String managemobile(Model model, HttpSession session) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-        model.addAttribute("dto", dto);
-        return "managemobile";
-    }
-    
-    
-    @PostMapping("/managemobile")
-    public String postManagemobile(
-            @RequestParam("password") String password,
-            @RequestParam("newPhoneNumber") String newPhoneNumber,
-            RedirectAttributes redirectAttributes,
-            Model model,
-            HttpSession session
-            ) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-
-        // 현재 비밀번호와 저장된 비밀번호를 비교하여 일치하는지 확인
-        if (passwordEncoder.matches(password, dto.getPassword())) {
-            // 새 비밀번호와 확인 비밀번호가 일치하는지 확인
-            query.updateMobile(newPhoneNumber, dto.getPassword());
-                // 새 비밀번호를 암호화하여 저장
-                dto.setMobile(newPhoneNumber);
-                model.addAttribute("dto", dto);
-                redirectAttributes.addAttribute("message", "변경완료");
-                return "redirect:/info/mypage";
-            }
-        model.addAttribute("dto", dto);
-        redirectAttributes.addAttribute("message", "변경실패");
-        return "redirect:/info/managemobile";
-    }
-    
-
-    @GetMapping("/checkAddress")
-    public String checkAddress(Model model, HttpSession session) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-        model.addAttribute("dto", dto);
-        return "checkAddress";
-    }
-    @PostMapping("/checkAddress")
-    public String PostcheckAddress(Model model, HttpSession session) {
-	MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-	model.addAttribute("dto", dto);
-	return "redirect:/info/manageaddress";
-    }
-    
-    @GetMapping("/manageaddress")
-    public String manageaddress(Model model, HttpSession session) {
-        MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-        model.addAttribute("dto", dto);
-        return "manageaddress";
-    }
-    
-    @PostMapping("/manageaddress")
-    public String postManageaddress(
-	    @RequestParam("newPostalCode") String newPostalCode,
-            @RequestParam("newAddressBasic") String newAddressBasic,
-            @RequestParam("newAddressRest") String newAddressRest,
-            @RequestParam("password") String password,
-	    Model model, HttpSession session,
-	    RedirectAttributes redirectAttributes) {
-	 MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-	if (passwordEncoder.matches(password, dto.getPassword())) {
-	    query.updatepostalCode(newPostalCode, dto.getPassword());
-	    query.updateAddressBasic(newAddressBasic, dto.getPassword());
-	    query.updateAddressRest(newAddressRest, dto.getPassword());
-	    dto.setPostalCode(newPostalCode);
-	    dto.setAddressBasic(newAddressBasic);
-	    dto.setAddressRest(newAddressRest);
-	    
-	    redirectAttributes.addAttribute("message","completeUpdateAd");
-	    return "redirect:/info/checkAddress";
+	@Autowired // passwordEncoder 주입
+	public ModifyMemberInfoController(MemberQuery query, PasswordEncoder passwordEncoder) {
+		this.query = query;
+		this.passwordEncoder = passwordEncoder;
 	}
-	model.addAttribute("dto",dto);
-	redirectAttributes.addAttribute("message","fail");
-        return "redirect:/info/checkAddress";
-    }
-    @GetMapping("/withdraw")
-    public String withdraw(Model model, HttpSession session) {
-	 MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-	 model.addAttribute("dto", dto);
-	return "withdraw";
-    }
-    @PostMapping("/withdraw")
-    public String postWithdraw(
-	    @RequestParam("email") String email,
-	    @RequestParam("password") String password,
-	    Model model, HttpSession session,
-	    RedirectAttributes redirectAttributes) {
-	MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
-	if (passwordEncoder.matches(password, dto.getPassword())) {
-	    query.remove(dto.getPassword(), dto.getEmail());
-	    model.addAttribute("dto",dto);
-	    redirectAttributes.addAttribute("message", "탈퇴완료");
-	    return "redirect:/my/main";
-	}else if (!passwordEncoder.matches(password, dto.getPassword())) {
-	    redirectAttributes.addAttribute("message","탈퇴실패");
-	    return "redirect:/info/withdraw";
+
+	@GetMapping("/mypage")
+	public String mypage(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "mypage";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "mypage";
+		}
+		return "mypage";
 	}
-	return "withdraw";
+
+	@GetMapping("/mypage2")
+	public String mypage2(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "mypage2";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "mypage2";
+		}
+
+		return "mypage2";
+	}
+
+	@GetMapping("/manageacc")
+	public String manageacc(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "manageacc";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "manageacc";
+		}
+
+		return "manageacc";
+	}
+
+	@GetMapping("/manageaccSocial")
+	public String manageaccSocial(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "manageaccSocial";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "manageaccSocial";
+		}
+
+		return "manageaccSocial";
+	}
+
+	@GetMapping("/myinfo")
+	public String myinfo(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "myinfo";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			System.err.println("dto.getAddress : " + userProfile.getAddressBasic());
+			return "myinfo";
+		}
+
+		return "myinfo";
+	}
+
+	@GetMapping("/managepw")
+	public String managepw(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "managepw";
+		} 
+
+		return "managepw";
+	}
+
+	@PostMapping("/managepw")
+	public String changePw(@RequestParam("password") String password, @RequestParam("newPassword") String newPassword,
+			@RequestParam("confirmPassword") String confirmPassword, Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
+
+			// 현재 비밀번호와 저장된 비밀번호를 비교하여 일치하는지 확인
+			if (passwordEncoder.matches(password, dto.getPassword())) {
+				// 새 비밀번호와 확인 비밀번호가 일치하는지 확인
+				if (newPassword.equals(confirmPassword)) {
+					// 새 비밀번호를 암호화하여 저장
+					String hashedNewPassword = passwordEncoder.encode(newPassword);
+					query.updatePassword(hashedNewPassword, dto.getPassword());
+					dto.setPassword(hashedNewPassword);
+					model.addAttribute("dto", dto);
+					session.setAttribute("dto", dto);
+
+					// 비밀번호 변경 성공 시 로그아웃 후 메인 페이지로 리다이렉트
+					return "redirect:/view/login";
+				} else {
+					model.addAttribute("dto", dto);
+					return "redirect:/info/managepw";
+				}
+			}
+		}
+		return "managepw";
+	}
+
+	@GetMapping("/managemobile")
+	public String managemobile(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+		if (dtoObject instanceof MemberJoinDto) {
+
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "managemobile";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "managemobile";
+		}
+		return "managemobile";
+	}
+
+	@PostMapping("/managemobile")
+	public String postManagemobile(@RequestParam("newPhoneNumber") String newPhoneNumber,
+			RedirectAttributes redirectAttributes, Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+		if (dtoObject instanceof MemberJoinDto) {
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			query.updateMobileByMobile(newPhoneNumber,dto.getEmail());
+			dto.setMobile(newPhoneNumber);
+			redirectAttributes.addAttribute("message", "변경완료");
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "redirect:/info/mypage";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			query.updateMobileByMobile(newPhoneNumber,userProfile.getEmail());
+			userProfile.setMobile(newPhoneNumber);
+			model.addAttribute("dto", userProfile);
+			redirectAttributes.addAttribute("message", "변경완료");
+			session.setAttribute("dto", userProfile);
+			return "redirect:/info/mypage2";
+		} else {
+
+			return "redirect:/info/managemobile";
+		}
+	}
+
+	@GetMapping("/checkAddress")
+	public String checkAddress(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+		if (dtoObject instanceof MemberJoinDto) {
+
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "checkAddress";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "checkAddress";
+		}
+		return "checkAddress";
+	}
+
+	@PostMapping("/checkAddress")
+	public String PostcheckAddress(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+		if (dtoObject instanceof MemberJoinDto) {
+
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "redirect:/info/manageaddress";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "redirect:/info/manageaddress";
+		}
+		return "redirect:/info/manageaddress";
+	}
+
+	@GetMapping("/manageaddress")
+	public String manageaddress(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+		if (dtoObject instanceof MemberJoinDto) {
+
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "manageaddress";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+			return "manageaddress";
+		}
+		return "manageaddress";
+	}
+
+	@PostMapping("/manageaddress")
+	public String postManageaddress(@RequestParam("newPostalCode") String newPostalCode,
+			@RequestParam("newAddressBasic") String newAddressBasic,
+			@RequestParam("newAddressRest") String newAddressRest,
+			Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+		
+		Object dtoObject = session.getAttribute("dto");
+		if (dtoObject instanceof MemberJoinDto) {
+
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			query.updatepostalCode(newPostalCode,dto.getEmail());
+			query.updateAddressBasic(newAddressBasic,dto.getEmail());
+			query.updateAddressRest(newAddressRest,dto.getEmail());
+			dto.setPostalCode(newPostalCode);
+			dto.setAddressBasic(newAddressBasic);
+			dto.setAddressRest(newAddressRest);
+
+			redirectAttributes.addAttribute("message", "completeUpdateAd");
+			model.addAttribute("dto", dto);
+			session.setAttribute("dto", dto);
+			return "redirect:/info/checkAddress";
+		}else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			query.updatepostalCode(newPostalCode,userProfile.getEmail());
+			query.updateAddressBasic(newAddressBasic,userProfile.getEmail());
+			query.updateAddressRest(newAddressRest,userProfile.getEmail());
+			userProfile.setPostalCode(newPostalCode);
+			userProfile.setAddressBasic(newAddressBasic);
+			userProfile.setAddressRest(newAddressRest);
+
+			model.addAttribute("dto", userProfile);
+			session.setAttribute("dto", userProfile);
+		}
+		return "redirect:/info/checkAddress";
+	}
+		
+		
 	
-	
-    }
+
+
+	@GetMapping("/withdraw")
+	public String withdraw(Model model, HttpSession session) {
+		Object dtoObject = session.getAttribute("dto");
+		if (dtoObject instanceof MemberJoinDto) {
+
+			MemberJoinDto dto = (MemberJoinDto) dtoObject;
+			model.addAttribute("dto", dto);
+			return "withdraw";
+		} else if (dtoObject instanceof UserProfile) {
+			UserProfile userProfile = (UserProfile) dtoObject;
+			model.addAttribute("dto", userProfile);
+			return "withdraw";
+		}
+		return "withdraw";
+	}
+
+	@PostMapping("/withdraw")
+	public String postWithdraw(@RequestParam("email") String email, @RequestParam("password") String password,
+			Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+		MemberJoinDto dto = (MemberJoinDto) session.getAttribute("dto");
+		if (passwordEncoder.matches(password, dto.getPassword())) {
+			query.remove(dto.getPassword(), dto.getEmail());
+			model.addAttribute("dto", dto);
+			redirectAttributes.addAttribute("message", "탈퇴완료");
+			return "redirect:/my/main";
+		} else if (!passwordEncoder.matches(password, dto.getPassword())) {
+			redirectAttributes.addAttribute("message", "탈퇴실패");
+			return "redirect:/info/withdraw";
+		}
+		return "withdraw";
+
+	}
 }
--- WebSite/src/main/java/com/fullstack2/website/controller/ViewController.java
+++ WebSite/src/main/java/com/fullstack2/website/controller/ViewController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,69 +25,104 @@ import com.fullstack2.webSite.dtos.MemberJoinDto;
 import com.fullstack2.webSite.entity.Member;
 import com.fullstack2.webSite.repository.MemberQuery;
 import com.fullstack2.webSite.service.MemberService;
+import com.fullstack2.webSite.oauth2.OAuth2Service;
+import com.fullstack2.webSite.oauth2.UserProfile;
+import com.nimbusds.oauth2.sdk.token.AccessToken;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/view")
 public class ViewController {
+
+    @Autowired
+    private MemberQuery query;
+    @Autowired
+    private OAuth2Service oAuth2Service; // OAuth2Service 주입 추가
+    MemberJoinDto dto = new MemberJoinDto();
+    
+    Member member = new Member();
+    MemberService memberService;
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @GetMapping("/join")
+    public String joinPage() {
+	return "join";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+	return "admin";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+	return "login";
+    }
+
+    @GetMapping("/mainLog")
+    public String dashboardPage(@AuthenticationPrincipal User user, Model model, HttpSession session) {
+	if (user != null) {
+	    String email = user.getUsername();
+
+	    dto.setEmail(email);
+	    dto.setPassword(query.selectPw(email));
+	    dto.setName(query.selectName(email));
+	    dto.setProvider(query.selectProvider(email));
+	    // 주소
+	    dto.setPostalCode(query.selectpostalCode(email));
+	    dto.setAddressBasic(query.selectaddressBasic(email));
+	    dto.setAddressRest(query.selectaddressRest(email));
+	    // 핸드폰 번호
+	    dto.setMobile(query.selectPh(email));
+	    // 생일
+	    dto.setBirth(query.selectBirth(email));
+	    // 권한
+	    dto.setRole(query.selectRole(email));
+
+	    System.out.println("mainLog dto:" + dto);
+	    session.setAttribute("dto", dto);
+	    model.addAttribute("dto", dto);
+	    return "mainLog";
+	} 
 	
-	@Autowired
-	private MemberQuery query;
-	MemberJoinDto dto = new MemberJoinDto();
-	Member member = new Member();
-	MemberService memberService;
-	 @Autowired
-	    private PasswordEncoder encoder;
+	return "mainLog";
 
-	@GetMapping("/login")
-	public String loginPage() {
-		
-		return "login";
-	}
+    }
+    @GetMapping("/mainLog2")
+    public String dashboardPage2(@AuthenticationPrincipal User user, Model model, HttpSession session) {
+    	if (user == null) {
+    		
+    		UserProfile userProfile = (UserProfile) session.getAttribute("dto");
+    	    userProfile.setPostalCode(query.selectpostalCode(userProfile.getEmail()));
+    	    userProfile.setAddressBasic(query.selectaddressBasic(userProfile.getEmail()));
+    	    userProfile.setAddressRest(query.selectaddressRest(userProfile.getEmail()));
+    	    userProfile.setProvider(query.selectProvider(userProfile.getEmail()));
+    	    // 핸드폰 번호
+    	    userProfile.setMobile(query.selectPh(userProfile.getEmail()));
+    	    // 생일
+    	    userProfile.setBirth(query.selectBirth(userProfile.getEmail()));
+    	    // 권한
+    	    userProfile.setRole(query.selectRole(userProfile.getEmail()));
+    	    System.err.println("mainLog dto:" + userProfile.getAddressBasic());
+    	    System.err.println("mainLog dto:" + userProfile.getEmail());
+    	    session.setAttribute("dto", userProfile);
+    	    model.addAttribute("dto", userProfile);
+    	}
+    	return "mainLog2";
+    	
+    }
 
-	@GetMapping("/join")
-	public String joinPage() {
-		return "join";
-	}
+    @GetMapping("/setting/admin")
+    @AdminAuthorize
+    public String adminSettingPage() {
+	return "admin_setting";
+    }
 
-	
-	
-	//여기서 유저 정보 모두 끌어오기
-	@GetMapping("/mainLog")
-	public String dashboardPage(@AuthenticationPrincipal User user, Model model,HttpSession session) {
-		String email = user.getUsername();
-		
-		dto.setEmail(email);
-		dto.setPassword(query.selectPw(email));
-		dto.setName(query.selectName(email));
-		
-		//주소
-		dto.setPostalCode(query.selectpostalCode(email));
-		dto.setAddressBasic(query.selectaddressBasic(email));
-		dto.setAddressRest(query.selectaddressRest(email));
-		//핸드폰 번호
-		dto.setMobile(query.selectPh(email));
-		//생일
-		dto.setBirth(query.selectBirth(email));
-		// 권한
-		dto.setRole(query.selectRole(email));
-		
-		System.out.println("mainLog dto:"+ dto);
-		session.setAttribute("dto", dto);
-		model.addAttribute("dto", dto);
-		return "mainLog";
-	}
-
-	@GetMapping("/setting/admin")
-	@AdminAuthorize
-	public String adminSettingPage() {
-		return "admin_setting";
-	}
-
-	@GetMapping("/setting/user")
-	@UserAuthorize
-	public String userSettingPage() {
-		return "user_setting";
-	}
+    @GetMapping("/setting/user")
+    @UserAuthorize
+    public String userSettingPage() {
+	return "user_setting";
+    }
 }
