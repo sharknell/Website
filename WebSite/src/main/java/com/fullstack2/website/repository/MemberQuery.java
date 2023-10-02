6f783fe84636a470c78f471d1243d61f8a4756b4
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-10-02 13:16:51 +0900

    Add files via upload

--- WebSite/src/main/java/com/fullstack2/website/repository/MemberQuery.java
+++ WebSite/src/main/java/com/fullstack2/website/repository/MemberQuery.java
@@ -12,12 +12,31 @@ import jakarta.transaction.Transactional;
 public interface MemberQuery extends JpaRepository<Member, String> {
     
     
-	@Query("SELECT m.email FROM Member m WHERE m.email = :email")
-	void selectEmail(@Param("email") String email);
+	@Query("SELECT m.email FROM Member m WHERE m.email = :insertEmail")
+	String selectEmail(@Param("insertEmail") String insertEmail);
+	
+	@Query("SELECT m.name FROM Member m WHERE m.name = :insertName")
+	String selectNameByName(@Param("insertName") String insertName);
+	
+	@Query("SELECT m.mobile FROM Member m WHERE m.mobile= :insertMobile")
+	String selectMobile(@Param("insertMobile") String insertMobile);
+	
+	@Query("SELECT m.email FROM Member m WHERE m.mobile= :insertMobile")
+	String selectMobilebyEmail(@Param("insertMobile") String insertMobile);
+	
 	@Transactional
 	@Modifying
 	@Query("UPDATE Member m SET m.password = :newPassword WHERE m.password = :pw ")
 	void updatePassword(@Param("newPassword") String newPassword ,@Param("pw") String pw);
+	@Transactional
+	@Modifying
+	@Query("UPDATE Member m SET m.password = :newPassword WHERE m.email = :newEmail ")
+	void updateUserPassword(@Param("newEmail") String newEmail ,@Param("newPassword") String newPassword);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE Member m SET m.mobile = :newMobile WHERE m.email = :Email ")
+	void updateMobileByMobile(@Param("newMobile") String newMobile, @Param("Email") String Email);
 	
 	 // 회원탈퇴
 	    @Transactional
@@ -30,23 +49,23 @@ public interface MemberQuery extends JpaRepository<Member, String> {
 	 // 배송지 변경
 	    @Transactional
 	    @Modifying
-	    @Query("update Member m SET m.addressRest = :NewaddressRest WHERE password = :confirmPassword")
-	    void updateAddressRest(@Param("NewaddressRest") String NewaddressRest,@Param("confirmPassword") String confirmPassword);
+	    @Query("update Member m SET m.addressRest = :NewaddressRest where m.email= :email")
+	    void updateAddressRest(@Param("NewaddressRest") String NewaddressRest,@Param("email") String email);
 	    @Transactional
 	    @Modifying
-	    @Query("update Member m SET m.addressBasic = :NewaddressBasic  WHERE password = :confirmPassword")
-	    void updateAddressBasic(@Param("NewaddressBasic") String NewaddressBasic,@Param("confirmPassword") String confirmPassword);
+	    @Query("update Member m SET m.addressBasic = :NewaddressBasic  where m.email= :email")
+	    void updateAddressBasic(@Param("NewaddressBasic") String NewaddressBasic,@Param("email") String email);
 	    @Transactional
 	    @Modifying
-	    @Query("update Member m SET m.postalCode = :NewpostalCode WHERE password = :confirmPassword")
-	    void updatepostalCode(@Param("NewpostalCode") String NewpostalCode,@Param("confirmPassword") String confirmPassword);
+	    @Query("update Member m SET m.postalCode = :NewpostalCode  where m.email= :email")
+	    void updatepostalCode(@Param("NewpostalCode") String NewpostalCode,@Param("email") String email);
 	    
 	    
 	 // 전화번호 변경
 	    @Transactional
 	    @Modifying
-	    @Query("UPDATE Member m SET m.mobile = :newMobile WHERE m.password = :password")
-	    void updateMobile(@Param("newMobile") String newMobile, @Param("password") String password);
+	    @Query("UPDATE Member m SET m.mobile = :newMobile ")
+	    void updateMobile(@Param("newMobile") String newMobile);
 	
 	// db 땡겨오기
 	@Query("SELECT m.mobile FROM Member m WHERE m.email = :email ")
@@ -65,6 +84,8 @@ public interface MemberQuery extends JpaRepository<Member, String> {
 	String selectpostalCode(@Param("email") String email);
 	@Query("SELECT m.role FROM Member m WHERE m.email = :email ")
 	String selectRole(@Param("email") String email);
+	@Query("SELECT m.provider FROM Member m WHERE m.email = :email ")
+	String selectProvider(@Param("email") String email);
   
     @Query("SELECT m.id FROM Member m WHERE m.email = :email")
     long selectId(@Param("email") String email);
