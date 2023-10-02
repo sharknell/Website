c1657dda16599a8112c6a2d4fc80852c6b432ba9
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-10-02 13:15:27 +0900

    Add files via upload

--- WebSite/src/main/java/com/fullstack2/website/entity/Member.java
+++ WebSite/src/main/java/com/fullstack2/website/entity/Member.java
@@ -9,12 +9,14 @@ import lombok.NoArgsConstructor;
 
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Builder
-public class Member extends BaseEntity{
+public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,6 +25,7 @@ public class Member extends BaseEntity{
     private String email;
     private String password;
     private String name;
+    
     // 주소
    
     private String postalCode;
@@ -33,11 +36,71 @@ public class Member extends BaseEntity{
     private String mobile;
     private String birth;
     private String role;
+    private String provider;
     
     
  // 회원가입 시 목록 추가할 곳
-    public static Member createUser(String email, String password, String name, String postalCode, String addressBasic, String addressRest, String phone, String mobile, String birth, PasswordEncoder passwordEncoder) {
-        return new Member(null, email, passwordEncoder.encode(password), name, postalCode, addressBasic, addressRest, phone, mobile, birth,"USER" );
+   /* public static Member createMember(
+	    String email, 
+	    String password,
+	    String name,
+	    String postalCode, 
+	    String addressBasic, 
+	    String addressRest,
+	    String phone, 
+	    String mobile,
+	    String birth,
+	    String role,
+	    String provider, 
+	    PasswordEncoder passwordEncoder) {
+        return new Member(null,email, passwordEncoder.encode(password), name, postalCode, addressBasic, addressRest, phone, mobile, birth,"Member","일반회원" );
+    }*/
+    public static Member createMember(
+	    String email, 
+	    String password,
+	    String name,
+	    String postalCode, 
+	    String addressBasic, 
+	    String addressRest,
+	    String phone, 
+	    String mobile,
+	    String birth,
+	    
+	    PasswordEncoder passwordEncoder) {
+	    Member member = new Member();
+	    member.setEmail(email);
+	    member.setPassword(passwordEncoder.encode(password));
+	    member.setName(name);
+	    member.setPostalCode(postalCode);
+	    member.setAddressBasic(addressBasic);
+	    member.setAddressRest(addressRest);
+	    member.setPhone(phone);
+	    member.setMobile(mobile);
+	    member.setBirth(birth);
+	    member.setRole("USER");
+	    member.setProvider("일반회원");
+	    return member;
+	}
+
+    
+    
+    
+    
+    // 사용자의 이름이나 이메일을 업데이트하는 메소드
+    public Member updateUser(String name, String email, String birth, String mobile) {
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+        this.mobile = mobile;
+        
+        return this;
+    }
+    public Member updateUserGoogle(String name, String email) {
+	this.name = name;
+	this.email = email;
+	
+	
+	return this;
     }
 
    
